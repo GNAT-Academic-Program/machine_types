b8969dd13c6fae5a02dea3c32fe228cae34e8816
Olivier Henley <olivier.henley@gmail.com>
2026-03-25 19:48:38 -0400

    first commit

---
+++ tests/common/machine_types_tests.ads
@@ -0,0 +1,8 @@
+pragma Warnings (Off);
+with Ada.Assertions; use Ada.Assertions;
+--  Make Assert visible to children
+pragma Warnings (On);
+
+package Machine_Types_Tests is
+
+end Machine_Types_Tests;
